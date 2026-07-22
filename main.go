72790cbd61086d0299d155db911605be45d6cdb2
Hiroshi Umehara <zinrai0x19@gmail.com>
2026-07-22 12:32:01 +0900

    Add version subcommand consuming injected ldflags

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ import (
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "version" {
+		printVersion()
+		return
+	}
+
 	serverMode := flag.Bool("server", false, "Run in server mode")
 	flag.Usage = func() {
 		fmt.Println("debiface-gen: Debian Network Interface Configuration Generator")
